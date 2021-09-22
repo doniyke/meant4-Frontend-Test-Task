5e7058a0a3c17debe525e71fefc395f9d9993086
Iyke <iykedavis69@gmail.comgit config --global user.name Iykegit config --global user.email iykedavis69@gmail.com>
2021-09-22 15:10:50 +0100

    Project Upload : Created project, created form component, sent form data to api, created tests using jest

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,41 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="container-fluid">
+      	<div class="rounded shadow p-md-5 p-3 my-3 m-md-5">
+          	<div class="row">
+				<div class="col-md-6">
+					<div>
+						<img src="@/assets/contact.svg" alt="" class="img-fluid">
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="rounded shadow bg-light p-md-5 p-3 form-holder">
+						<h3>Reach Out To Us</h3>
+						<p>We can't wait to hear form you</p>
+						<Form />
+					</div>
+				</div>
+			</div>
+      	</div>
+    </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
+import Form from '@/components/Form';
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Form
   }
 }
 </script>
+<style scoped>
+	.form-holder{
+		color: #999;
+	}
+	.form-holder h3{
+		color: #000;
+		font-weight: 900;
+	}
+</style>
